0c3a309beeade493f8051371826b0b53e31215a3
Thomas Ward <teward@thomas-ward.net>
2024-05-11 19:17:42 -0400

    Add SDXL Common Resolutions

--- prompt.py
+++ prompt.py
@@ -3,7 +3,7 @@ from .base import GLOBAL_CATEGORY, BaseNode
 MODULE_CATEGORY = f"{GLOBAL_CATEGORY}"
 
 
-class HelperNode_Prompt(BaseNode):
+class HelperNodes_Prompt(BaseNode):
     """
     This is a multi-field TEXT node that allows entering a positive and negative
     prompt and pass them both out.
